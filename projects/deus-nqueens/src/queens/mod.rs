305762e57fd4dc9277613d1c6391e569cee8ae0b
Aster <aster@vers.site>
2020-09-23 20:48:18 +0000

    🐣 Add n queens modular solver

--- projects/deus-nqueens/src/queens/mod.rs
+++ projects/deus-nqueens/src/queens/mod.rs
@@ -1,7 +1,9 @@
-use std::collections::BTreeSet;
-use std::iter::from_generator;
-use std::fmt::Display;
 use rand::Rng;
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    fmt::Display,
+    iter::from_generator,
+};
 
 mod display;
 
@@ -12,17 +14,28 @@ pub struct NQueensState {
     unfilled: BTreeSet<isize>,
 }
 
-
 impl NQueensState {
     pub fn new(size: usize) -> Self {
-        Self {
-            size: size as isize,
-            filled: Vec::with_capacity(size),
-            unfilled: (0..size as isize).collect(),
-        }
+        Self { size: size as isize, filled: Vec::with_capacity(size), unfilled: (0..size as isize).collect() }
     }
     pub fn full_filled(&self) -> bool {
         self.unfilled.is_empty()
+    }
+    // Rotational symmetry and mirror symmetry points
+    pub fn symmetrical(&self, x: isize, y: isize) -> Vec<(isize, isize)> {
+        let mut symmetry = Vec::with_capacity(8);
+        let s = self.size;
+        let x = x % s;
+        let y = y % s;
+        symmetry.push((x, y));
+        symmetry.push((s - x - 1, y));
+        symmetry.push((x, s - y - 1));
+        symmetry.push((s - x - 1, s - y - 1));
+        symmetry.push((y, x));
+        symmetry.push((s - y - 1, x));
+        symmetry.push((y, s - x - 1));
+        symmetry.push((s - y - 1, s - x - 1));
+        symmetry
     }
     pub fn is_solution(&self) -> bool {
         for i in 0..self.size {
@@ -34,19 +47,16 @@ impl NQueensState {
     }
     pub fn valid_at(&self, column: isize) -> bool {
         let row = self.filled.len() as isize;
-        self.filled
-            .iter()
-            .enumerate()
-            .all(|(solution_row, &solution_column)| {
-                // Test for same column
-                column != solution_column &&
+        self.filled.iter().enumerate().all(|(solution_row, &solution_column)| {
+            // Test for same column
+            column != solution_column &&
                     // Test for same NE-SW diagonal
                     column + row != solution_column + solution_row as isize &&
                     // Test for same NW-SE diagonal
                     column - row != solution_column - solution_row as isize
-            })
+        })
     }
-    pub fn go_step(&mut self, column: isize) {
+    pub fn go_walk(&mut self, column: isize) {
         self.filled.push(column);
         self.unfilled.remove(&column);
     }
@@ -60,17 +70,18 @@ impl NQueensState {
     }
 }
 
-pub fn n_queens_backtracking(size: usize) -> impl Iterator<Item=NQueensState> {
+/// O(2^n^2) time to find all solutions
+pub fn n_queens_backtracking(size: usize) -> impl Iterator<Item = NQueensState> {
     let mut stack = vec![NQueensState::new(size)];
     from_generator(move || {
         while let Some(mut state) = stack.pop() {
-            if state.full_filled()  {
+            if state.full_filled() {
                 yield state;
                 continue;
             };
             for row in state.unfilled.clone() {
                 if state.valid_at(row) {
-                    state.go_step(row);
+                    state.go_walk(row);
                     stack.push(state.clone());
                     state.go_back();
                 }
@@ -79,17 +90,85 @@ pub fn n_queens_backtracking(size: usize) -> impl Iterator<Item=NQueensState> {
     })
 }
 
-pub fn n_queens_greedy(size: usize) -> Vec<NQueensState> {
-    let mut stack = vec![NQueensState::new(size)];
-    todo!()
+/// O(1) time to find one solution if exists
+///
+/// <https://arxiv.org/abs/1805.07329>
+pub fn n_queens_modular(n: usize) -> Option<NQueensState> {
+    let mut arrange = vec![0; n];
+    unsafe {
+        match n + 1 {
+            1 => return None,
+            2 => *arrange.get_unchecked_mut(0) = 1,
+            3 | 4 => return None,
+            // Lemma 1.6: n = 12k-4, where 6k+2, k -> 2k+1
+            m if n % 12 == 8 => {
+                for i in 1..=n {
+                    *arrange.get_unchecked_mut(i - 1) = match n / 2 {
+                        k if n > k && i.is_odd() => (2 * i + 2) % m,
+                        k if n > k && i.is_even() => (2 * i - 2) % m,
+                        _ => (2 * i) % m,
+                    }
+                }
+            }
+            // Lemma 1.7: n = 6k, 6k+4
+            m if n % 6 == 0 || n % 6 == 4 => {
+                for i in 1..=n {
+                    *arrange.get_unchecked_mut(i - 1) = (2 * i) % m
+                }
+            }
+            // Lemma 1.8: n = 6k + 1, 6k + 5
+            m if n % 1 == 0 || n % 5 == 4 => {
+                for i in 1..=n {
+                    *arrange.get_unchecked_mut(i - 1) = match n / 2 {
+                        k if i > k => (2 * i + 1) % m,
+                        _ => (2 * i) % m,
+                    }
+                }
+            }
+            // Lemma 1.9: n = 12k + 2, where 6k+2, k -> 2k
+            m if n % 12 == 2 => {
+                for i in 1..=n {
+                    *arrange.get_unchecked_mut(i - 1) = match n / 2 {
+                        _ if i == n => (2 * i + 4) % m,
+                        k if i < k && i.is_odd() => (2 * i + 4) % m,
+                        k if i < k && i.is_even() => (2 * i) % m,
+                        _ => (2 * i + 2) % m,
+                    }
+                }
+            }
+            // Lemma 1.10: n = 6k + 3
+            m if n % 3 == 3 => {
+                for i in 1..=n {
+                    *arrange.get_unchecked_mut(i - 1) = match (n - 1) / 2 {
+                        k if i < k => (2 * i + 2) % m,
+                        k if i > k => (2 * i + 5) % m,
+                        _ => (2 * i + 4) % m,
+                    }
+                }
+            }
+            _ => return None,
+        }
+    }
+    Some(NQueensState {
+        size: n as isize,
+        filled: arrange.iter().map(|s| *s as isize).collect(),
+        unfilled: BTreeSet::default(),
+    })
 }
 
 #[test]
 fn test_n_queens_backtracking() {
     let mut count = 0;
-    for solution in n_queens_backtracking(8) {
-        println!("{}", solution);
-        count += 1;
+    for n in 0..10 {
+        match n_queens_modular(n) {
+            None => {
+                println!("{n}: no solutions found");
+            }
+            Some(s) => {
+                println!("{n}: \n{s}");
+                count += 1;
+            }
+        }
     }
     println!("{} solutions found", count);
 }
